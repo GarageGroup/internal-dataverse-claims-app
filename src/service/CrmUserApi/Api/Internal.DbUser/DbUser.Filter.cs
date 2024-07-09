545ea1bcb46a6e581ce53787d57a1bd67c51b62c
Pavel Moskovoy <p@moskovoi.ru>
2024-07-09 14:59:45 +0300

    Update DbUser.Filter.cs

--- src/service/CrmUserApi/Api/Internal.DbUser/DbUser.Filter.cs
+++ src/service/CrmUserApi/Api/Internal.DbUser/DbUser.Filter.cs
@@ -6,7 +6,8 @@ partial record class DbUser
 {
     internal readonly static DbRawFilter DefaultFilter
         =
-        new($"{AliasName}.title IS NOT NULL " +
+        new(
+            $"{AliasName}.title IS NOT NULL " +
             $"AND {AliasName}.isdisabled = 0 " +
             $"AND {AliasName}.azureactivedirectoryobjectid IS NOT NULL");
 }
